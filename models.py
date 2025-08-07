686d515bf0429f31d383fb61b91ddb78a35dde10
StevenLomon <steven.lennartsson@gmail.com>
2025-08-07 08:33:53 +0200

    Uncommenting the Focus Block relationship in DailyIntention

--- models.py
+++ models.py
@@ -55,7 +55,7 @@ class DailyIntention(Base):
     # Relationships
     user = relationship("User", back_populates="daily_intentions")
     daily_results = relationship("DailyResult", back_populates="daily_intention", uselist=False) # One-to-one relationship with DailyResult
-    # focus_blocks = relationship("FocusBlock", back_populates="daily_intention") #For individual focus block tracking
+    focus_blocks = relationship("FocusBlock", back_populates="daily_intention") #For individual focus block tracking
 
 class FocusBlock(Base):
     __tablename__ = "focus_blocks"
