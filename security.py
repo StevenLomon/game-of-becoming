3140668e187dff095b73cde026c318a15f1cd584
StevenLomon <steven.lennartsson@gmail.com>
2025-08-08 10:05:44 +0200

    JWT Token Handling variables and functions

--- security.py
+++ security.py
@@ -1,5 +1,11 @@
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Optional
+
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+# --- Password Hashing ---
 # Create a CryptContext instance; tells passlib to use bcrypt for hasing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -10,3 +16,23 @@ def verify_password(plain_password, hashed_password):
 # Function to hash a plain password
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+# --- JWT (Token) Handling ---
+# In production, load this from an env variable
+SECRET_KEY = os.getenv("SECRET_KEY", "a_very_secret_key_for_dev")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 # A JWT token is valid for 30 minutes
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        # Default expiration time in none is provided
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    # The 'sub' (subject) claim is standard for storing the user identifier
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
