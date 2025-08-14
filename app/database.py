82fe7d75ab2b5f6ffcb84388e18509c8943d4811
StevenLomon <steven.lennartsson@gmail.com>
2025-08-14 14:52:49 +0200

    database.py cleaned from  SQLAlchemy 1.4 leftovers

--- app/database.py
+++ app/database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker
 import os
 
 # Database setup
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./game_of_becoming.db")
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 # Dependency generator to get the database session
 def get_db():
