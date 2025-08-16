2d956684118c80c1673c0f55241abd4a641e8b2e
StevenLomon <steven.lennartsson@gmail.com>
2025-08-16 12:58:32 +0200

    Proxy between 5173 and 8000 set up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Forward any request starting with /api to our FastAPI backend
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        // Optional but recommended: remove /api prefix before sending to backend
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 })
