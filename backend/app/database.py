b2a03cdae62e1d64d37c081ff02ce26dfd3f5a69
StevenLomon <steven.lennartsson@gmail.com>
2025-08-18 16:49:22 +0200

    alembic.ini and database.py both updated to use explicit path rather than relative path; our db file should be in the backend folder!

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Database setup
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./game_of_becoming.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///backend/game_of_becoming.db")
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
