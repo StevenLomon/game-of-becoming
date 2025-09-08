1adca968cc2862007c409b0738efbe75c519f448
StevenLomon <steven.lennartsson@gmail.com>
2025-09-08 15:12:34 +0200

    Teaching Tailwind our pulsing orb animation!

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'pulse-orb': {
+          '0%, 100%': { opacity: '1', transform: 'scale(1)' },
+          '50%': { opacity: '0.5', transform: 'scale(0.75)' },
+        }
+      },
+      animation: {
+        'pulse-orb': 'pulse-orb 1.5s infinite ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
